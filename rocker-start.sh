44b9d8552f39f8cfb28cdcf03a9c5a560f233115
AlexBorochkin <aaborochkin@gmail.com>
2018-12-13 21:10:21 +0300

    shell scripts removed to another folder

--- rocker-start.sh
+++
@@ -1,2 +0,0 @@
-docker-compose -f /home/$USER/r-projects/rstudio/docker-image/docker-compose.yml up -d --remove-orphans
-
--- rocker-stop.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f /home/$USER/r-projects/rstudio/docker-image/docker-compose.yml down
