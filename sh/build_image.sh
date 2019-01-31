cdd17a83dcd6bce8513907b406d8e88506dc4856
AlexBorochkin <aaborochkin@gmail.com>
2019-01-31 10:12:46 +0300

    shell script inserted inside jenkins file

--- sh/build_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GIT_VERSION=$(git describe --tags | sed s/v//)
-docker-compose build
-docker tag $USER/rstudio:latest $USER/rstudio:$GIT_VERSION
-docker tag $USER/rstudio-finance:latest $USER/rstudio-finance:$GIT_VERSION
-docker tag $USER/rstudio-text:latest $USER/rstudio-text:$GIT_VERSION
-docker system prune -f
